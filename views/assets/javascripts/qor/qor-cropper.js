29e78837de939ab67acdd65551d38d3961468b06
jasonweng <wengjianm@gmail.com>
2020-10-15 15:07:19 +0800

    fix(cropper) fix crop image bug for first upload

--- views/assets/javascripts/qor/qor-cropper.js
+++ views/assets/javascripts/qor/qor-cropper.js
@@ -407,11 +407,11 @@
                 sizeData = $target.data(),
                 sizeName = sizeData.sizeName || 'original',
                 sizeResolution = sizeData.sizeResolution,
-                originalUrl = /\.original\./.test(sizeData.originalUrl)
+                originalUrl = (sizeData && sizeData.originalUrl) ? $target.attr('src') : (/\.original\./.test(sizeData.originalUrl)
                     ? sizeData.originalUrl
                     : /\.original\./.test($target.attr('data-original-url'))
                     ? $target.attr('data-original-url')
-                    : $target.attr('data-original-url').replace(/file\./, 'file.original.'),
+                    : $target.attr('data-original-url').replace(/file\./, 'file.original.')),
                 $clone = $(`<img src=${originalUrl}>`),
                 data = this.data || {},
                 _this = this,
